b15695d6cbc400d597978d70f25d48f3fb1861c1
toaxdjr <47952703+toaxdjr@users.noreply.github.com>
2022-05-22 17:01:30 -0500

    Update VMC4UEBlueprintFunctionLibrary.cpp

--- VMC4UE/Source/VMC4UE/Source/VMC4UEBlueprintFunctionLibrary.cpp
+++ VMC4UE/Source/VMC4UE/Source/VMC4UEBlueprintFunctionLibrary.cpp
@@ -16,6 +16,7 @@ void UVMC4UEBlueprintFunctionLibrary::OnReceivedVMC(UVMC4UEStreamingSkeletalMesh
 	}
 
     const FString AddressString = Address.ToString();
+	float scale = SkeletalMeshTransform->Scale;
 
 	if (AddressString == TEXT("/VMC/Ext/Root/Pos"))
 	{
@@ -37,11 +38,14 @@ void UVMC4UEBlueprintFunctionLibrary::OnReceivedVMC(UVMC4UEStreamingSkeletalMesh
 		const auto UnityRotationW = Data[Index++].FloatValue;
 
 		auto& VMCBone = SkeletalMeshTransform->Root;
-
-		VMCBone.Location.X = -UnityLocationX * 100.0f;
-		VMCBone.Location.Y = UnityLocationZ * 100.0f;
-		VMCBone.Location.Z = UnityLocationY * 100.0f;
-
+		if (0.0f != scale)
+		{
+			VMCBone.Location.X = -UnityLocationX * scale;
+			VMCBone.Location.Y = UnityLocationZ * scale;
+			VMCBone.Location.Z = UnityLocationY * scale;
+		}
+		else {
+		}
 		VMCBone.Rotation.X = -UnityRotationX;
 		VMCBone.Rotation.Y = UnityRotationZ;
 		VMCBone.Rotation.Z = UnityRotationY;
@@ -71,11 +75,12 @@ void UVMC4UEBlueprintFunctionLibrary::OnReceivedVMC(UVMC4UEStreamingSkeletalMesh
         const auto UnityRotationW = Data[Index++].FloatValue;
 
 		auto& VMCBone = SkeletalMeshTransform->Bones.FindOrAdd(BoneName);
-
-		VMCBone.Location.X = -UnityLocationX * 100.0f;
-		VMCBone.Location.Y = UnityLocationZ * 100.0f;
-		VMCBone.Location.Z = UnityLocationY * 100.0f;
-
+		if (0.0f != scale)
+		{
+			VMCBone.Location.X = -UnityLocationX * scale;
+			VMCBone.Location.Y = UnityLocationZ * scale;
+			VMCBone.Location.Z = UnityLocationY * scale;
+		}
 		VMCBone.Rotation.X = -UnityRotationX;
 		VMCBone.Rotation.Y = UnityRotationZ;
 		VMCBone.Rotation.Z = UnityRotationY;
@@ -85,18 +90,23 @@ void UVMC4UEBlueprintFunctionLibrary::OnReceivedVMC(UVMC4UEStreamingSkeletalMesh
 	}
 	else if (AddressString == TEXT("/VMC/Ext/Blend/Val"))
 	{
+
 		if (Data.Num() < 2)
 		{
+			//UE_LOG(LogTemp, Log, TEXT("No blendshapes"));
 			return;
 		}
 		int32 Index = 0;
+		FString pop = AddressString;
 
+		//UE_LOG(LogTemp, Log, TEXT("yes blendshapes %s : %d"), pop,);
 		const auto Name = Data[Index++].StringValue;
 		const auto Value = Data[Index++].FloatValue;
-
+		//UE_LOG(LogTemp, Log, TEXT("yes blendshapes  %f"), Value);
 		auto& TargetBlendShape = SkeletalMeshTransform->FutureBlendShapes.FindOrAdd(Name);
 
 		TargetBlendShape = Value;
+		
 	}
 	else if (AddressString == TEXT("/VMC/Ext/Blend/Apply"))
 	{
@@ -104,7 +114,10 @@ void UVMC4UEBlueprintFunctionLibrary::OnReceivedVMC(UVMC4UEStreamingSkeletalMesh
 		
 		SkeletalMeshTransform->CurrentBlendShapes.Empty();
 		for (auto& FutureBlendShape : SkeletalMeshTransform->FutureBlendShapes)
-		{
+		{   /*
+			if (FutureBlendShape.Key == "eyeBlinkLeft") {
+				UE_LOG(LogTemp, Log, TEXT("Value for left eye %f"), FutureBlendShape.Value);
+			}*/
 			SkeletalMeshTransform->CurrentBlendShapes.FindOrAdd(FutureBlendShape.Key);
 			SkeletalMeshTransform->CurrentBlendShapes[FutureBlendShape.Key] = FutureBlendShape.Value;
 		}
@@ -138,7 +151,6 @@ TWeakObjectPtr<UVMC4UEStreamingSkeletalMeshTransform> UVMC4UEBlueprintFunctionLi
 			return *StreamingSkeletalMeshTransform;
 		}
 		UVMC4UEStreamingSkeletalMeshTransform* NewStreamingSkeletalMeshTransform = NewObject<UVMC4UEStreamingSkeletalMeshTransform>();
-
 		//FRWScopeLock RWScopeLock2(NewStreamingSkeletalMeshTransform->RWLock, FRWScopeLockType::SLT_Write);
 		OSCManager->StreamingSkeletalMeshTransformMap.Emplace(Port, NewStreamingSkeletalMeshTransform);
 
@@ -164,17 +176,27 @@ void UVMC4UEBlueprintFunctionLibrary::RefreshConnection(float Seconds)
 
 	Seconds = FGenericPlatformMath::Min(Seconds, 1.0f);
 
+
 	// Reconnect
 	{
 		FRWScopeLock RWScopeLock(OSCManager->RWLock, FRWScopeLockType::SLT_ReadOnly);
 
 		auto Now = UKismetMathLibrary::Now();
+		OSCManager->ResetReceiverCallbacks();
 		
 		for (auto& OscReceiver : OSCManager->OscReceivers)
 		{
+			UE_LOG(LogTemp, Log, TEXT("Port: %d health check\n"), OscReceiver->GetPort());
 			auto Span = Now - OscReceiver->GetLastUpdateTime();
+			UE_LOG(LogTemp, Log, TEXT("Seconds since last msg: %ld\n"), Span.GetTotalSeconds());
 			if (Span.GetTotalSeconds() > (double)Seconds)
 			{
+				
+				UE_LOG(LogTemp, Log, TEXT("Trying to reconnect: %d\n"), OscReceiver->GetPort());
+				OscReceiver->Reconnect();
+			}
+			if (Span.GetTotalSeconds() < (double)0.0f) {
+				UE_LOG(LogTemp, Log, TEXT("Trying to reconnect messages from the future?: %d\n"), OscReceiver->GetPort());
 				OscReceiver->Reconnect();
 			}
 		}
