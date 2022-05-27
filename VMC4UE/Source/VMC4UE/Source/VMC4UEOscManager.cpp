d799853ffa2307cc82fa69d35d3ef5d955bd3f35
toaxdjr <47952703+toaxdjr@users.noreply.github.com>
2022-05-27 09:36:22 -0500

    Update VMC4UEOscManager.cpp

--- VMC4UE/Source/VMC4UE/Source/VMC4UEOscManager.cpp
+++ VMC4UE/Source/VMC4UE/Source/VMC4UEOscManager.cpp
@@ -8,30 +8,11 @@ UVMC4UEOSCManager* UVMC4UEOSCManager::Instance = nullptr;
 
 UVMC4UEOSCManager* UVMC4UEOSCManager::GetInstance()
 {
-	if (Instance == nullptr)
+	if (!IsValid(Instance))
 	{
 		Instance = NewObject< UVMC4UEOSCManager >();
 		check(Instance)
 		Instance->AddToRoot();
 	}
 	return Instance;
-}
-
-//added function to restart hanging OSC threads
-void UVMC4UEOSCManager::ResetReceiverCallbacks()
-{
-	if (Instance == nullptr)
-	{
-		Instance = NewObject< UVMC4UEOSCManager >();
-		check(Instance)
-		Instance->AddToRoot();	
-	}
-	
-	for (auto& OscReceiver : Instance->OscReceivers) {
-			for (auto& ISSMTM : Instance->StreamingSkeletalMeshTransformMap) {
-				if (ISSMTM.Key == OscReceiver->GetPort() && !OscReceiver->OSCReceiveEventDelegate.IsBound()) {
-					OscReceiver->OSCReceiveEventDelegate.AddDynamic(ISSMTM.Value, &UVMC4UEStreamingSkeletalMeshTransform::OnReceived);
-				}
-			}
-	}
 }
